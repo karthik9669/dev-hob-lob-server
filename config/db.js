79cffa5664007a7c4de124fcf2824cbec9f53939
Karthik <karthikyalamati@gmail.com>
2022-07-26 09:58:22 +0530

    adding to github

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+require('dotenv').config();
+const db = process.env.MONGOURI;
+
+// Connection to MongoDB
+console.log("gogle")
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+      // useFindAndModify: false,
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
